6bd61fc53cdfe5563a490d23cd18f2035672ca51
wloodar <wlodardev@gmail.com>
2023-10-05 00:41:30 +0200

    removed imagetools plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 
-import { astroImageTools } from 'astro-imagetools'
+// import { astroImageTools } from 'astro-imagetools'
 
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), astroImageTools],
+    integrations: [mdx(), sitemap()],
     image: {
         domains: ['images.unsplash.com'],
     },
